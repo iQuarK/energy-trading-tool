684752923f8188ce69b5f3c7b96806c6861af090
Luis Sanchez Castellanos <iquark@gmail.com>
2024-05-24 03:53:07 +0200

    Added a comment in store

--- src/app/data/store.tsx
+++ src/app/data/store.tsx
@@ -6,6 +6,8 @@ export const subject$ = new BehaviorSubject<EnergyOfferingFields[]>([]);
 const initialState: EnergyOfferingFields[] = [];
 let state: EnergyOfferingFields[] = [];
 
+// Here the store is encapsulated to get all the functionalities we need
+// sucha as init with the data coming from the JSON file, upsert operation, etc.
 export const store = {
   init: (data: EnergyOfferingFields[]) => {
     state = data ?? initialState;
@@ -13,7 +15,7 @@ export const store = {
   },
   subscribe: (setState: Dispatch<SetStateAction<EnergyOfferingFields[]>>) =>
     subject$.subscribe(setState),
-  
+
   upsert: (energyOffering: EnergyOfferingFields) => {
     const index = state.findIndex((item) => item.type === energyOffering.type);
 
